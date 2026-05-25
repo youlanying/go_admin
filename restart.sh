72f84f4551115e94ac18271bcbf4c8be54042d42
youlanying <489503787@qq.com>
2026-05-26 00:03:21 +0800

    同步

--- restart.sh
+++ restart.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
+set -e
 echo "go build"
 go mod tidy
-go build -o go-admin main.go
+CGO_ENABLED=1 go build -tags sqlite3 -o go-admin main.go
 chmod +x ./go-admin
 echo "kill go-admin service"
-killall go-admin # kill go-admin service
-nohup ./go-admin server -c=config/settings.dev.yml >> access.log 2>&1 & #后台启动服务将日志写入access.log文件
-echo "run go-admin success"
-ps -aux | grep go-admin
+killall go-admin 2>/dev/null || true
+echo "run go-admin"
+exec ./go-admin server -c=config/settings.sqlite.yml
